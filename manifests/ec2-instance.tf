e0eb45a995ee0086325474ac1337b8cc38b38dcf
rnalus24 <rnalus24@gmail.com>
2024-05-22 12:29:08 +0800

    2nd push

--- manifests/ec2-instance.tf
+++ manifests/ec2-instance.tf
@@ -2,16 +2,25 @@ terraform {
   required_providers {
     aws = {
         source = "hashicorp/aws"
-        #version = "value"
+        #version = "~> 2.0"
     }
   }
 
-  required_version = "~> 2.0"
+  required_version = "~> 1.0"
 }
 
 provider "aws" {
   profile = "default"
   region = "ap-southeast-1"
+}
+
+resource "aws_vpc" "myvpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "myvpc1"
+  }
 }
 
 resource "aws_instance" "myec2vm" {
