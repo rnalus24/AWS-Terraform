26f1217822e27b97e400008aca5eab4f70acdaed
rnalus24 <rnalus24@gmail.com>
2024-05-22 12:36:12 +0800

    3rd push

--- manifests/ec2-instance.tf
+++ manifests/ec2-instance.tf
@@ -5,7 +5,6 @@ terraform {
         #version = "~> 2.0"
     }
   }
-
   required_version = "~> 1.0"
 }
 
@@ -14,6 +13,13 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
+provider "aws" {
+  profile = "default"
+  region = "us-east-1"
+  alias = "us"
+}
+
+/*
 resource "aws_vpc" "myvpc" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
@@ -22,8 +28,10 @@ resource "aws_vpc" "myvpc" {
     Name = "myvpc1"
   }
 }
+*/
 
 resource "aws_instance" "myec2vm" {
-  ami = "ami-0ac9397cab55f5044"
+  ami = "ami-0bb84b8ffd87024d8" #ami-0ac9397cab55f5044 SG AMI IDs
   instance_type = "t2.micro"
+  provider = aws.us
 }
